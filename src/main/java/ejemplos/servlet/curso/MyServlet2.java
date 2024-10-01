659c7ca0eafc5298e66b598370f6758bef96200c
clabrado <carlos.labrado@externos.mjusticia.es>
2024-10-01 16:07:53 +0200

    Envío del campo Nombre a través de un formulario por parámetros y a través del dispatcher y métido doPost en el Servlet 2

--- src/main/java/ejemplos/servlet/curso/MyServlet2.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet2.java
@@ -14,14 +14,43 @@ public class MyServlet2 extends HttpServlet {
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 
+		// Recoge el parámetro desde la URL
+		//String inputText = request.getParameter("fname");
+
+
+
+		// Recibe el dato enviado por el primer servlet
+		String inputText = (String) request.getAttribute("fname");
+
+
+
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 
 		// send HTML page to client
 		out.println("<html>");
-		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
+		out.println("<head><title>Recogida de parámetro de Servlet 1</title></head>");
 		out.println("<body>");
-		out.println("<h1>Bienvenido!!</h1>");
+		out.println("<h1>Bienvenid@ " + inputText +"!</h1>");
+	}
+
+	protected void doPost(HttpServletRequest request,
+						 HttpServletResponse response) throws ServletException, IOException {
+
+
+		// Recibe el dato enviado por el primer servlet
+		String inputText = (String) request.getAttribute("fname");
+
+
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		// send HTML page to client
+		out.println("<html>");
+		out.println("<head><title>Recogida de parámetro de Servlet 1</title></head>");
+		out.println("<body>");
+		out.println("<h1>Bienvenid@ " + inputText +"!</h1>");
 	}
 
 }
