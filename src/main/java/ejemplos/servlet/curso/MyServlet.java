a6947fe37190f8a6529b9817c64aab56085b5338
clabrado <carlos.labrado@externos.mjusticia.es>
2024-10-01 16:07:53 +0200

    Envío del campo Nombre a través de un formulario por parámetros

--- src/main/java/ejemplos/servlet/curso/MyServlet.java
+++ src/main/java/ejemplos/servlet/curso/MyServlet.java
@@ -19,12 +19,27 @@ public class MyServlet extends HttpServlet {
 
 		// send HTML page to client
 		out.println("<html>");
-		out.println("<head><title>Ejemplo HTML desde Servlet</title></head>");
+		out.println("<head><title>Formulario HTML</title></head>");
 		out.println("<body>");
-		out.println("<h1>Ejemplo Servlet</h1>");
-		out.println("<p>Este es un ejemplo en el curso de Java para generar HTML desde un Servlet.</p>");
-		out.println("<p><a href=\"/myServlet2\">Vamos al otro Servlet</a></p>");
+		out.println("<h1>Actividad 1 Servlets</h1>");
+		out.println("<form action='myServlet' method='POST'>");
+		out.println("<label for=\"fname\">Nombre:</label>");
+		out.println("<input type=\"text\" id=\"fname\" name=\"fname\" value=\"\">");
+		out.println("<input type=\"submit\" value=\"Submit\">");
+		out.println("</form>");
 		out.println("</body></html>");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Recibe el valor del formulario
+		String inputText = request.getParameter("fname");
+
+		// Añade el dato como atributo para el siguiente servlet
+		request.setAttribute("fname", inputText);
+
+		// Redirige al segundo servlet, pasando el parámetro en la URL
+		response.sendRedirect("myServlet2?fname=" + inputText);
 	}
 
 }
